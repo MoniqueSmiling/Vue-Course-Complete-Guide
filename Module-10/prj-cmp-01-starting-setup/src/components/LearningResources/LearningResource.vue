e9f54f926ad3b7824ddbf2c58d075f3d355b7626
Lola Jahn <lolajahn@gmail.com>
2021-02-12 20:29:09 +0100

    Beginning module 10

---
+++ Module-10/prj-cmp-01-starting-setup/src/components/LearningResources/LearningResource.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <header>
+      <div>
+        <h2>{{ title }}</h2>
+      </div>
+      <button>Delete</button>
+    </header>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link'],
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 1.2rem;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
---
+++ Module-10/prj-cmp-01-starting-setup/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+
+import App from './App.vue';
+
+const app = createApp(App);
+
+// import LearningResource from './components/LearningResources/LearningResource.vue';
+
+// app.component('learning-resource', LearningResource)
+
+app.mount('#app');
